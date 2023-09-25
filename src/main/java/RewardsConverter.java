6068d05421fcc0a07418efd7c45df489ebc8e78f
mehak aggarwal <mehakaggarwal037@gmail.com>
2023-09-25 10:40:31 +0530

    added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,4 +1,26 @@
 import java.util.Scanner;
+class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
 
 public class RewardsConverter {
     public static void main(String[] args) {
